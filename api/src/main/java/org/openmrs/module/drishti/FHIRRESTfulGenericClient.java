2ae0afc25619dd2778e015e410fddc4e03c5abb1
Bell Eapen <github@gulfdoctor.net>
2018-12-07 10:18:31 -0500

    Before Dao

---
+++ api/src/main/java/org/openmrs/module/drishti/FHIRRESTfulGenericClient.java
@@ -0,0 +1,39 @@
+package org.openmrs.module.drishti;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.CarePlan;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Reference;
+
+public class FHIRRESTfulGenericClient {
+
+    private static final FhirContext ctx = FhirContext.forDstu3();
+
+
+    public Bundle getBundleClient(org.openmrs.Patient patient) {
+        IGenericClient client = ctx.newRestfulGenericClient(DrishtiConstants.FHIR_BASE);
+        Bundle bundle = client
+                .search()
+                .forResource(Bundle.class)
+                .where(Bundle.IDENTIFIER.exactly().systemAndIdentifier(DrishtiConstants.URN_SYSTEM, patient.getUuid()))
+                //.where(Observation.SUBJECT.hasId(patient.getId()))
+                .returnBundle(org.hl7.fhir.dstu3.model.Bundle.class)
+                .execute();
+        return bundle;
+    }
+
+
+    public Boolean saveCareplanClient(CarePlan carePlan, Patient patient) {
+        IGenericClient client = ctx.newRestfulGenericClient(DrishtiConstants.FHIR_BASE);
+        carePlan.setSubject(new Reference(patient));
+        MethodOutcome outcome = client.create()
+                .resource(carePlan)
+                .prettyPrint()
+                .encodedJson()
+                .execute();
+        return outcome.getCreated();
+    }
+}
--- api/src/main/java/org/openmrs/module/drishti/api/impl/DrishtiServiceImpl.java
+++ api/src/main/java/org/openmrs/module/drishti/api/impl/DrishtiServiceImpl.java
@@ -9,78 +9,29 @@
  */
 package org.openmrs.module.drishti.api.impl;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.api.MethodOutcome;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.CarePlan;
 import org.hl7.fhir.dstu3.model.Patient;
-import org.hl7.fhir.dstu3.model.Reference;
-import org.openmrs.api.APIException;
-import org.openmrs.api.UserService;
 import org.openmrs.api.impl.BaseOpenmrsService;
-import org.openmrs.module.drishti.DrishtiConstants;
-import org.openmrs.module.drishti.Item;
+import org.openmrs.module.drishti.FHIRRESTfulGenericClient;
 import org.openmrs.module.drishti.api.DrishtiService;
-import org.openmrs.module.drishti.api.dao.DrishtiDao;
 
 public class DrishtiServiceImpl extends BaseOpenmrsService implements DrishtiService {
-	
-	DrishtiDao dao;
-
-	UserService userService;
 
 
-	IGenericClient client = FhirContext.forDstu3().newRestfulGenericClient(DrishtiConstants.FHIR_BASE);
+	FHIRRESTfulGenericClient fhirresTfulGenericClient = new FHIRRESTfulGenericClient();
 
 	/**
 	 * Injected in moduleApplicationContext.xml
 	 */
-	public void setDao(DrishtiDao dao) {
-		this.dao = dao;
-	}
-
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-
-	@Override
-	public Item getItemByUuid(String uuid) throws APIException {
-		return dao.getItemByUuid(uuid);
-	}
-
-	@Override
-	public Item saveItem(Item item) throws APIException {
-		if (item.getOwner() == null) {
-			item.setOwner(userService.getUser(1));
-		}
-
-		return dao.saveItem(item);
-	}
 
 	@Override
     public Bundle getBundle(org.openmrs.Patient patient) {
-        Bundle bundle = client
-				.search()
-                .forResource(Bundle.class)
-				.where(Bundle.IDENTIFIER.exactly().systemAndIdentifier(DrishtiConstants.URN_SYSTEM, patient.getUuid()))
-                //.where(Observation.SUBJECT.hasId(patient.getId()))
-				.returnBundle(org.hl7.fhir.dstu3.model.Bundle.class)
-				.execute();
-        return bundle;
+		return fhirresTfulGenericClient.getBundleClient(patient);
 	}
 
 	@Override
     public Boolean saveCareplan(CarePlan carePlan, Patient patient) {
-        carePlan.setSubject(new Reference(patient));
-        MethodOutcome outcome = client.create()
-                .resource(carePlan)
-                .prettyPrint()
-                .encodedJson()
-                .execute();
-        return outcome.getCreated();
+		return fhirresTfulGenericClient.saveCareplanClient(carePlan, patient);
 	}
 }
