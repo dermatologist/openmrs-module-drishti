3fe000f71a275861839c3d97ab7f7e695eb848ad
dermatologist <github@gulfdoctor.net>
2018-12-12 17:45:59 -0500

    Required previledges to view patients.

--- api/src/main/java/org/openmrs/module/drishti/DrishtiConstants.java
+++ api/src/main/java/org/openmrs/module/drishti/DrishtiConstants.java
@@ -2,8 +2,8 @@ package org.openmrs.module.drishti;
 
 public class DrishtiConstants {
 
-	public static final String FHIR_BASE = "http://tomcat.nuchange.ca/fhir";
+    public static final String FHIR_BASE = "http://tomcat.nuchange.ca/fhir";
 
-	public static final String URN_SYSTEM = "urn:system";
+    public static final String URN_SYSTEM = "urn:system";
 	
 }
--- omod/src/main/java/org/openmrs/module/drishti/fragment/controller/HgraphFragmentController.java
+++ omod/src/main/java/org/openmrs/module/drishti/fragment/controller/HgraphFragmentController.java
@@ -14,8 +14,8 @@ import java.util.List;
 
 public class HgraphFragmentController {
 
-    public void controller(FragmentConfiguration config, @SpringBean("patientService") PatientService patientService,
-                           FragmentModel model) throws Exception {
+	public void controller(FragmentConfiguration config, @SpringBean("patientService") PatientService patientService,
+						   FragmentModel model) throws Exception {
 		// unfortunately in OpenMRS 2.1 the coreapps patient page only gives us a patientId for this extension point
 		// (not a patient) but I assume we'll fix this to pass patient, so I'll code defensively
 		Patient patient;
@@ -27,9 +27,9 @@ public class HgraphFragmentController {
 			patient = (Patient) (pt instanceof Patient ? pt : PropertyUtils.getProperty(pt, "patient"));
 		DrishtiService drishtiService = Context.getService(DrishtiService.class);
 
-        Bundle bundle = drishtiService.getBundle(patient);
+		Bundle bundle = drishtiService.getBundle(patient);
 
-        int steps = 0;
+		int steps = 0;
 		for (Bundle.BundleEntryComponent bundleEntryComponent : bundle.getEntry()) {
 			Resource resource = bundleEntryComponent.getResource();
 			if (resource instanceof Observation) {
@@ -38,19 +38,19 @@ public class HgraphFragmentController {
 				for (Coding code : codes) {
 					if (code.getCode().equals("55423-8")) {
 
-                        List<Observation.ObservationComponentComponent> components = ((Observation) resource).getComponent();
+						List<Observation.ObservationComponentComponent> components = ((Observation) resource).getComponent();
 						for (Observation.ObservationComponentComponent component : components) {
 							Quantity quantity = component.getValueQuantity();
-                            //quantity.getValue() returns BigDecimal that is immutable
-                            steps += quantity.getValue().intValue();
+							//quantity.getValue() returns BigDecimal that is immutable
+							steps += quantity.getValue().intValue();
 						}
 
-                    }
+					}
 				}
 			}
 		}
 
-        model.addAttribute("patient", patient);
-        model.addAttribute("steps", steps);
+		model.addAttribute("patient", patient);
+		model.addAttribute("steps", steps);
 	}
 }
